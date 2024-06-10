b249ae8ab36c1e69444d150c2de8fd02add3a86e
aliasghar gholami <144837096+aliasghardev@users.noreply.github.com>
2024-06-10 15:23:43 +0330

    Add files via upload

---
+++ assets/channel-Cu4KmlNJ.js
@@ -0,0 +1,2 @@
+import{am as n,an as o}from"./index-Cndpunf_.js";const t=(a,r)=>n.lang.round(o.parse(a)[r]);export{t as c};
+//# sourceMappingURL=channel-Cu4KmlNJ.js.map
---
+++ assets/clone-BQq6z4fy.js
@@ -0,0 +1,2 @@
+import{a as r}from"./graph-BlnIXC9p.js";var a=4;function n(o){return r(o,a)}export{n as c};
+//# sourceMappingURL=clone-BQq6z4fy.js.map
